b4b9667a7678e187c0473f42ac67a9892f3ab08e
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-01-16 20:34:36 +0600

    aidar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React , {useState, Suspense} from "react";
-import Home from "./pages/Home/Home";
-import './scss/style.scss'
-import Layout from "./Layout/Layout";
-import About from "./pages/Home/About";
-import { Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound/NotFound";
-
+import Header from './layout/header/header';
+import Deliver from './pages/deliver/deliver';
+import './sass/style.scss'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route path="" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Route>
-    </Routes>
+    <>
+    <Header/>
+    <main>
+      <Deliver/>
+    </main>
+    </>
   );
 }
 
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer/Footer';
-import Header from "./Header/Header";
+import React from 'react'
 
 const Layout = () => {
-    return (
-        <>
-            <Header/>
-            {/* <Home/> */}
-            <Outlet/>
-            <Footer/>
-            
-        </>
-    );
-};
+  return (
+    <div>
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
