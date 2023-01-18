877f54025bf1670c266bd9454477a49af9272285
Kim Kamilla <kamila258568@gmail.com>
2023-01-18 18:12:51 +0300

    elean

---
+++ src/data/aboutData.js
@@ -0,0 +1,13 @@
+import Icon1 from '../assets/about/1.svg'
+import Icon2 from '../assets/about/2.svg'
+import Icon3 from '../assets/about/3.svg'
+import Icon4 from '../assets/about/4.svg'
+import Icon5 from '../assets/about/5.svg'
+
+export const aboutData = [
+    {id: 1, text: 'Уровень пошива приравнивается к индивидуальному;', icon: Icon1},
+    {id: 2, text: 'Создаем лекала для костюмов, которые подчеркивают достоинства и скрывают недостатки фигуры;', icon: Icon2},
+    {id: 3, text: 'Качественное исполнение: ровные отстрочки, красивая обработка – вы можете носить вещи даже изнаночной стороной;', icon: Icon3},
+    {id: 4, text: 'Используем современное оборудование и качественные материалы для изготовления изделий;', icon: Icon4},
+    {id: 5, text: 'Наши швеи занимаются пошивом и конструированием одежды более 20 лет. За это время их навыки доведены до идеала... ', icon: Icon5}
+]
