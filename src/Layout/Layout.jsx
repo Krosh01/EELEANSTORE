4d5f6e459682235d6702889b8d087cc6e622b613
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-01-19 12:35:18 +0600

    navbar scroll

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './header/header'
+import HeaderStatic from './header/headerStatic'
 
 const Layout = () => {
   return (
     <>
     <Header/>
     <Outlet/>
+    <HeaderStatic/>
     </>
   )
 }
