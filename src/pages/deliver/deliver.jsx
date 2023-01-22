d0fd3b134b1bcb73feb195bc270f75b2b007da10
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-01-22 17:07:09 +0600

    Some change

--- src/pages/deliver/deliver.jsx
+++ src/pages/deliver/deliver.jsx
@@ -114,7 +114,7 @@ const Deliver = () => {
                   <div className='deliver__questions-object'><input className='deliver__questions-object_inp' type="text" placeholder='Введите имя' required/></div>
                   <div className='deliver__questions-object'><input className='deliver__questions-object_inp' type="number" placeholder='Введите номер' required/></div>
                   <div className='deliver__questions-object'><input className='deliver__questions-object_inp' type="email" placeholder='Введите свой email' required/></div>
-                  <textarea className='deliver__questions-object_textarea' name="textArea" id="" data-minLength="5" rows="3" placeholder='Сообщение'></textarea>
+                  <textarea className='deliver__questions-object_textarea' name="textArea" data-minLength="5" rows="3" placeholder='Сообщение'></textarea>
                   <button className='deliver__questions-btn' type='submit'>Отправить</button>
                 </form>
               </div>
