65c5327ba024d3ed0f584638ea6b9de256d3c47b
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-02-18 00:12:06 +0600

    bullshit

--- src/pages/MakeAnOrder/Makesense.jsx
+++ src/pages/MakeAnOrder/Makesense.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import store from '../../redux'
-import MakeAnOrder from './MakeAnOrder'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import MakeAnOrder from './MakeAnOrder';
 
 const Makesense = () => {
-    const makeOrder = useSelector((store) => store.makeOrder.makeOrder)
-  return (
-    <>
-     {makeOrder.map((products => <MakeAnOrder products={products} key={products.id}/>))} 
-    </>
-  )
-}
+  const makeOrder = useSelector((store) => store.makeOrder.makeOrder);
 
-export default Makesense
+  if (makeOrder.length === 0) {
+    return null; // Don't render anything if makeOrder is empty
+  }
+
+  const product = makeOrder[makeOrder.length - 1]; // Get the most recently added product
+
+  return <MakeAnOrder products={product} />;
+};
+
+export default Makesense;
